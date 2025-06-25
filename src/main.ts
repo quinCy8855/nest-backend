46bd3a1fb5a59a31a1d415cceadb19a24d61a09a
thanyaluknimnual8 <thanyaluknimnual8@gmail.com>
2025-06-25 11:04:31 +0700

    config port

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common'; // ✅ เพิ่มตรงนี้
-import { useContainer } from 'class-validator'; // ✅ ใช้เพื่อ resolve dependency injection ใน DTO (optional)
+import { ValidationPipe } from '@nestjs/common'; // เพิ่มตรงนี้
+import { useContainer } from 'class-validator'; // ใช้เพื่อ resolve dependency injection ใน DTO (optional)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ Setup ValidationPipe สำหรับ class-validator + class-transformer
+  //   Setup ValidationPipe สำหรับ class-validator + class-transformer
   app.useGlobalPipes(
     new ValidationPipe({
-      transform: true, // ✅ แปลง payload เป็น instance ของ class (ใช้ class-transformer)
-      whitelist: true, // ✅ ลบ field ที่ไม่อยู่ใน DTO
+      transform: true, //  แปลง payload เป็น instance ของ class (ใช้ class-transformer)
+      whitelist: true, //  ลบ field ที่ไม่อยู่ใน DTO
       forbidNonWhitelisted: true, // ❗ถ้ามี field แปลก ๆ จะ throw error
     }),
   );
 
-  // ✅ Optional: รองรับ Dependency Injection ใน class-validator
+    app.enableCors(); 
+  //  Optional: รองรับ Dependency Injection ใน class-validator
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   // Swagger config
@@ -30,6 +31,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
